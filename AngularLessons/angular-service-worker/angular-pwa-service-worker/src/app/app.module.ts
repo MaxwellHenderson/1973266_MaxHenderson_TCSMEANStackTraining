18c8592a579e20880d264129d9993fc1a42ed00d
Max <maxwell.blair.henderson@gmail.com>
2021-03-29 08:44:37 -0700

    Material and json

---
+++ AngularLessons/angular-service-worker/angular-pwa-service-worker/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonComponent } from './button/button.component';
+import { MatButtonModule } from '@angular/material/button';
+import { CardComponent } from './card/card.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { EmployeeComponent } from './employee/employee.component';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ButtonComponent,
+    CardComponent,
+    EmployeeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
