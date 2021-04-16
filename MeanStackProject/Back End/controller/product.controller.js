300bc1d248e02c94312828d2a89eb26da7b601c9
Max <maxwell.blair.henderson@gmail.com>
2021-04-16 08:38:03 -0700

    asdfadf

--- MeanStackProject/Back End/controller/product.controller.js
+++ MeanStackProject/Back End/controller/product.controller.js
@@ -9,4 +9,43 @@ let getProductDetails = (req, res) => {
   });
 };
 
-module.exports = { getProductDetails };
+let getProductById = (req, res) => {
+  let pid = req.params.pid;
+  ProductModel.find({ _id: pid }, (err, data) => {
+    if (!err) {
+      res.json(data);
+    }
+  });
+};
+
+let storeProductDetails = (req, res) => {
+  let product = new ProductModel({
+    _id: req.body.pid,
+    pname: req.body.pname,
+    price: req.body.price,
+  });
+  let newProduct = new ProductModel(product);
+  newProduct.save((err, result) => {
+    if (!err) {
+      res.send("Record stored successfully " + result);
+    } else {
+      res.send("Record didn't store " + err);
+    }
+  });
+};
+
+let deleteProductById = (req, res) => {
+  let pid = req.params.pid;
+  ProductModel.deleteOne({ _id: pid }, (err, data) => {
+    if (!err) {
+      res.json(data);
+    }
+  });
+};
+
+module.exports = {
+  getProductDetails,
+  getProductById,
+  storeProductDetails,
+  deleteProductById,
+};
--- MeanStackProject/Back End/router/product.router.js
+++ MeanStackProject/Back End/router/product.router.js
@@ -4,5 +4,8 @@ let ProductController = require("../controller/product.controller.js");
 
 //Mapping sub path with http methods
 router.get("/allProductDetails", ProductController.getProductDetails);
-
+router.get("/retrieveProductById/:pid", ProductController.getProductById);
+router.post("/storeProductDetails", ProductController.storeProductDetails);
+router.delete("/deleteProductById/:pid", ProductController.deleteProductById);
+// router.put("/updateProductPrice", ProductController.updateProductPrice);
 module.exports = router;
